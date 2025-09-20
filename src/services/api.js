3a34f30ad447ac7ef66f30d6f044138dec699b78
chittyk <kbarjun2468@gmail.com>
2025-09-20 23:11:28 +0530

    versel commit

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 // import { config } from "dotenv"
 
 
-const API_URL = import.meta.env.VITE_API_URL ||'http://localhost:8080'
+const API_URL = import.meta.env.VITE_API_URL 
 const api = axios.create({baseURL:API_URL})
 
 api.interceptors.request.use((config)=>{
